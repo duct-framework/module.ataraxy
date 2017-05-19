e3eaa09c7b044a15830b181419c4b98bcf23e943
James Reeves <jreeves@weavejester.com>
2017-05-19 04:32:55 +0100

    Change project name to module.ataraxy

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject duct/router.ataraxy "0.1.0-SNAPSHOT"
+(defproject duct/module.ataraxy "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
