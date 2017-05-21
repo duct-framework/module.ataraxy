fc4c60b6b4d06526d1e42b7a74ee4302a2c81676
James Reeves <jreeves@weavejester.com>
2017-05-21 22:16:00 +0100

    Release 0.1.1

--- README.md
+++ README.md
@@ -12,7 +12,7 @@ application.
 
 To install, add the following to your project `:dependencies`:
 
-    [duct/module.ataraxy "0.1.0"]
+    [duct/module.ataraxy "0.1.1"]
 
 ## Usage
 
--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject duct/module.ataraxy "0.1.0"
+(defproject duct/module.ataraxy "0.1.1"
   :description "Duct module and router for the Ataraxy routing library"
   :url "https://github.com/duct-framework/module.ataraxy"
   :license {:name "Eclipse Public License"
