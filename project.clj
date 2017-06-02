87feba891415af05a22895e740506150b3cb0f6b
James Reeves <jreeves@weavejester.com>
2017-06-02 21:20:03 +0100

    Update Ataraxy to 0.3.2

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0-alpha16"]
-                 [ataraxy "0.3.1"]
+                 [ataraxy "0.3.2"]
                  [duct/core "0.4.0"]
                  [integrant "0.4.0"]
                  [medley "1.0.0"]])
