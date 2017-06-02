5130eaecb52243743c94a1f315b4441dba526d91
James Reeves <jreeves@weavejester.com>
2017-06-02 21:18:03 +0100

    Update duct/core to 0.4.0

--- project.clj
+++ project.clj
@@ -5,6 +5,6 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0-alpha16"]
                  [ataraxy "0.3.1"]
-                 [duct/core "0.3.3"]
+                 [duct/core "0.4.0"]
                  [integrant "0.4.0"]
                  [medley "1.0.0"]])
