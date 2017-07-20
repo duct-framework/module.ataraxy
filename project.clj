c999ada2d82b58a4998a11043af6a4af56644c5a
James Reeves <jreeves@weavejester.com>
2017-07-20 22:45:03 +0200

    Update dependencies

--- project.clj
+++ project.clj
@@ -3,8 +3,8 @@
   :url "https://github.com/duct-framework/module.ataraxy"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0-alpha16"]
-                 [ataraxy "0.3.3"]
-                 [duct/core "0.4.0"]
-                 [integrant "0.4.0"]
+  :dependencies [[org.clojure/clojure "1.9.0-alpha17"]
+                 [ataraxy "0.4.0"]
+                 [duct/core "0.5.1"]
+                 [integrant "0.4.1"]
                  [medley "1.0.0"]])
