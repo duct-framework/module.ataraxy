b77a2f4d8b6a2f6981f6e7e4bb436b847663646d
James Reeves <jreeves@weavejester.com>
2018-09-23 15:24:18 +0100

    Add prep-key method to fill in missing handlers
    Possibly makes module redundant.

--- src/duct/router/ataraxy.clj
+++ src/duct/router/ataraxy.clj
@@ -1,6 +1,16 @@
 (ns duct.router.ataraxy
   (:require [ataraxy.core :as ataraxy]
+            [clojure.set :as set]
             [integrant.core :as ig]))
+
+(defn- missing-handlers [{:keys [routes handlers]}]
+  (set/difference (set (ataraxy/result-keys routes)) (set (keys handlers))))
+
+(defn- add-default-handlers [handler-map keys-to-add]
+  (reduce (fn [m k] (assoc m k (ig/ref k))) handler-map keys-to-add))
+
+(defmethod ig/prep-key :duct.router/ataraxy [_ options]
+  (update options :handlers add-default-handlers (missing-handlers options)))
 
 (defmethod ig/init-key :duct.router/ataraxy [_ options]
   (ataraxy/handler options))
--- test/duct/router/ataraxy_test.clj
+++ test/duct/router/ataraxy_test.clj
@@ -15,3 +15,22 @@
   (let [handler (:duct.router/ataraxy (ig/init config))]
     (is (= (handler {:request-method :get, :uri "/hello/world"})
            {:status 200, :headers {}, :body "Hello world"}))))
+
+(deftest prep-test
+  (let [routes '{[:get "/bar" id] [:foo/bar id]
+                 [:get "/baz"]    [:foo/baz]}]
+    (testing "no existing handlers"
+      (is (= (ig/prep {:duct.router/ataraxy {:routes routes}})
+             {:duct.router/ataraxy
+              {:routes   routes
+               :handlers {:foo/bar (ig/ref :foo/bar)
+                          :foo/baz (ig/ref :foo/baz)}}})))
+
+    (testing "some existing handlers"
+      (is (= (ig/prep {:duct.router/ataraxy
+                       {:routes   routes
+                        :handlers {:foo/bar (ig/ref :foo/custom)}}})
+             {:duct.router/ataraxy
+              {:routes   routes
+               :handlers {:foo/bar (ig/ref :foo/custom)
+                          :foo/baz (ig/ref :foo/baz)}}})))))
