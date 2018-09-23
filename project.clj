41bcd700c7d792e3deb4bb411a3da5d8eb0226ce
James Reeves <jreeves@weavejester.com>
2018-09-23 15:38:31 +0100

    Release 0.3.0-alpha2

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject duct/module.ataraxy "0.3.0-alpha1"
+(defproject duct/module.ataraxy "0.3.0-alpha2"
   :description "Duct module and router for the Ataraxy routing library"
   :url "https://github.com/duct-framework/module.ataraxy"
   :license {:name "Eclipse Public License"
