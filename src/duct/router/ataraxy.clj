aae9b30233c0be72d9a50530c8dcf22c4655b4f4
James Reeves <jreeves@weavejester.com>
2018-09-28 21:00:34 +0100

    Add duct/resource handling to router

--- src/duct/router/ataraxy.clj
+++ src/duct/router/ataraxy.clj
@@ -1,7 +1,14 @@
 (ns duct.router.ataraxy
   (:require [ataraxy.core :as ataraxy]
+            [ataraxy.response :as resp]
             [clojure.set :as set]
+            [clojure.java.io :as io]
+            [duct.core.resource]
             [integrant.core :as ig]))
+
+(extend-protocol resp/ToResponse
+  duct.core.resource.Resource
+  (->response [r] (resp/->response (io/as-url r))))
 
 (defn- missing-handlers [{:keys [routes handlers]}]
   (set/difference (set (ataraxy/result-keys routes)) (set (keys handlers))))
--- test/duct/router/ataraxy_test.clj
+++ test/duct/router/ataraxy_test.clj
@@ -1,18 +1,21 @@
 (ns duct.router.ataraxy-test
-  (:require [clojure.test :refer :all]
+  (:require [ataraxy.response :as response]
+            [clojure.test :refer :all]
+            [duct.core :as duct]
             [duct.router.ataraxy :as ataraxy]
             [integrant.core :as ig]))
 
 (defn hello-handler [{[_ name] :ataraxy/result}]
   {:status 200, :headers {}, :body (str "Hello " name)})
 
-(def config
-  {:duct.router/ataraxy
-   {:routes   '{[:get "/hello/" name] [:hello name]}
-    :handlers {:hello hello-handler}}})
+(defn resource-handler [_]
+  [::response/ok (duct/resource "duct/router/test.txt")])
 
 (deftest router-test
-  (let [handler (:duct.router/ataraxy (ig/init config))]
+  (let [config  {:duct.router/ataraxy
+                 {:routes   '{[:get "/hello/" name] [:hello name]}
+                  :handlers {:hello hello-handler}}}
+        handler (:duct.router/ataraxy (ig/init config))]
     (is (= (handler {:request-method :get, :uri "/hello/world"})
            {:status 200, :headers {}, :body "Hello world"}))))
 
@@ -34,3 +37,11 @@
               {:routes   routes
                :handlers {:foo/bar (ig/ref :foo/custom)
                           :foo/baz (ig/ref :foo/baz)}}})))))
+
+(deftest resource-test
+  (let [config   {:duct.router/ataraxy
+                  {:routes   '{[:get "/"] [:foo name]}
+                   :handlers {:foo resource-handler}}}
+        handler  (:duct.router/ataraxy (ig/init config))
+        response (handler {:request-method :get, :uri "/"})]
+    (is (= (-> response :body slurp) "Hello World\n"))))
